d7c9335d64fa2455a77bbe7304cfb26b0a02eae5
Jacob Smith <jacob.smith@eigeninnovations.com>
2018-05-01 18:10:45 -0300

    finished 7

---
+++ 1/7.cpp
@@ -0,0 +1,44 @@
+#include <assert.h>
+#include <iostream>
+#include <vector>
+#define print(x) std::cout << (x) << std::endl
+typedef std::vector<std::vector<int>> matrix;
+
+void rotate(matrix& image) {
+  int rows = image.size() - 1;
+  int cols = image[0].size() - 1;
+
+  int start = 0;
+  int end = image.size() - 1;
+  int level = 0;
+  while(true) {
+    if(start >= end) {
+      break;
+    }
+
+    for(int i = start; i < end; i++) {
+      int temp = image[level][i];
+      image[level][i] = image[rows-level][i];
+      image[rows-level][i] = image[rows-level][cols-i];
+      image[rows-level][cols-i] = image[level][cols-i];
+      image[level][cols-i] = temp;
+    }
+
+    start++;
+    end--;
+  }
+}
+
+int main() {
+  matrix image = {
+    {1, 2},
+    {3, 4}
+  };
+  matrix rotated = {
+    {3, 1},
+    {4, 2}
+  };
+
+  rotate(image);
+  assert(image == rotated);
+}
