94aa7e0ab0f6b7245d9e422971a137364b29def7
Jacob Smith <jacob.smith@eigeninnovations.com>
2018-05-06 11:35:42 -0300

    init chap 2 problems

---
+++ 2/1.cpp
@@ -0,0 +1,9 @@
+//
+// Write code to remove duplicates from an unsorted linked list.
+// How would you solve this problem if a temporary buffer is not allowed?
+//
+
+int main() {
+    return 0;
+}
+
---
+++ 2/2.cpp
@@ -0,0 +1,8 @@
+//
+// Implement an algorithm to find the kth to last element of a singly linked list.
+//
+
+int main() {
+    return 0;
+}
+
