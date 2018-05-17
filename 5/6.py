3736639794afb15eb708638a1cac0b28d29e72e4
Jacob Smith <jacob.smith@eigeninnovations.com>
2018-05-17 14:07:04 -0300

    init chap 5

---
+++ 5/6.py
@@ -0,0 +1,10 @@
+"""
+Write a function to determine the number of bits you would need to flip to convert
+integer A to integer B.
+
+EXAMPLE
+Input:
+Output:
+29 (or: 111131), 15 (or: 131111)
+2
+"""
